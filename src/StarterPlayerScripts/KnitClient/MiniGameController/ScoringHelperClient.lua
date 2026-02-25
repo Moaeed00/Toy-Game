14c951522305f2b1066fbf57d92934f76037f18b
GulraizRocketFuel <gulraiz.rocketfuel@gmail.com>
2026-02-25 15:42:18 +0500

    UPDATE score display format and ADD firecrackers VFX on goal

--- src/StarterPlayerScripts/KnitClient/MiniGameController/ScoringHelperClient.lua
+++ src/StarterPlayerScripts/KnitClient/MiniGameController/ScoringHelperClient.lua
@@ -8,20 +8,23 @@ Trove = Trove.new()
 
 local PlayerGui: PlayerGui = player:WaitForChild("PlayerGui")
 local ScoreGui: ScreenGui = PlayerGui:WaitForChild("ScoreGui")
-local ScoreValue: TextLabel = ScoreGui:WaitForChild("Main"):WaitForChild("Score")
-local OpponentScoreValue: TextLabel = ScoreGui:WaitForChild("Main"):WaitForChild("OpponentScore")
+local ScoreBG: ImageLabel = ScoreGui:WaitForChild("Main"):WaitForChild("ScoreBG")
+local ScoreValue: TextLabel = ScoreBG:WaitForChild("Score")
+local OpponentScoreBG: ImageLabel = ScoreGui:WaitForChild("Main"):WaitForChild("OpponentScoreBG")
+local OpponentScoreValue: TextLabel = OpponentScoreBG:WaitForChild("Score")
 local ComboValue: TextLabel = ScoreGui:WaitForChild("Main"):WaitForChild("Combo")
 
 local ScoringHelperClient = {}
 
 local function AddScore()
 	local UpdatedScore = player:GetAttribute("Score")
-	ScoreValue.Text = `Your Score {UpdatedScore}`
+	ScoreValue.Text = `{UpdatedScore}`
+	ScoringHelperClient:PlayFireCrackers()
 end
 
 local function AddOpponentScore()
 	local UpdatedScore = player:GetAttribute("OpponentScore")
-	OpponentScoreValue.Text = `Opponent Score {UpdatedScore}`
+	OpponentScoreValue.Text = `{UpdatedScore}`
 end
 
 local function AddCombo()
@@ -40,14 +43,37 @@ local function ResetCombo()
 	ComboValue.Text = 0
 end
 
+function ScoringHelperClient:PlayFireCrackers()
+	local duration = 2
+	local slotName = player:GetAttribute("MiniGameSlot")
+	local FireCrackers = workspace:WaitForChild("Toys"):WaitForChild(slotName):WaitForChild("FireCrackers"):GetChildren()
+	print("FireCrackers", FireCrackers)
+
+	for _, fire: Part in FireCrackers do
+		local emitter = fire:FindFirstChildOfClass("ParticleEmitter")
+		if emitter then
+			emitter.Enabled = true
+		end
+	end
+
+	task.delay(duration, function()
+		for _, fire: Part in FireCrackers do
+			local emitter = fire:FindFirstChildOfClass("ParticleEmitter")
+			if emitter then
+				emitter.Enabled = false
+			end
+		end
+	end)
+end
+
 function ScoringHelperClient:OnStartScoring(Challenge: boolean?)
-	ScoreValue.Text = "Your Score 0"
+	ScoreValue.Text = "0"
 	ScoreGui.Enabled = true
 	ComboValue.Visible = false
 
 	if Challenge then
-		OpponentScoreValue.Text = "Opponent Score 0"
-		OpponentScoreValue.Visible = true
+		OpponentScoreValue.Text = "0"
+		OpponentScoreBG.Visible = true
 	end
 end
 
@@ -70,9 +96,9 @@ end
 function ScoringHelperClient:CleanUp()
 	ScoreGui.Enabled = false
 	ComboValue.Visible = false
-	OpponentScoreValue.Visible = false
-	ScoreValue.Text = "Your Score 0"
-	OpponentScoreValue.Text = "Opponent Score 0"
+	OpponentScoreBG.Visible = false
+	ScoreValue.Text = "0"
+	OpponentScoreValue.Text = "0"
 	Trove:Destroy()
 end
 
