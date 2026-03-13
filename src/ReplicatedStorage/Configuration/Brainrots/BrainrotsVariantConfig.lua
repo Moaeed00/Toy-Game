4db7f1e1d060f9baefd0c6658ce281096578befe
GulraizRocketFuel <gulraiz.rocketfuel@gmail.com>
2026-03-13 10:09:01 +0500

    UPDATE remove brainrot variant consideration while searching for the brainrot being spawned

--- src/ReplicatedStorage/Configuration/Brainrots/BrainrotsVariantConfig.lua
+++ src/ReplicatedStorage/Configuration/Brainrots/BrainrotsVariantConfig.lua
@@ -1,22 +1,22 @@
 return {
 	VARIANTS = {{
-		Prefix = "Golden ",
+		Prefix = "Golden",
 		Chance = 0.08,
 		Color = Color3.fromRGB(255, 215, 0),
     }, {
-		Prefix = "Diamond ",
+		Prefix = "Diamond",
 		Chance = 0.04,
 		Color = Color3.fromRGB(0, 220, 255),
 	}, {
-		Prefix = "Lava ",
+		Prefix = "Lava",
 		Chance = 0.02,
 		Color = Color3.fromRGB(255, 80, 0),
 	}, {
-		Prefix = "Galaxy ",
+		Prefix = "Galaxy",
 		Chance = 0.01,
 		Color = Color3.fromRGB(150, 60, 255),
 	}, {
-		Prefix = "Rainbow ",
+		Prefix = "Rainbow",
 		Chance = 0.005,
 		Color = Color3.fromRGB(255, 255, 255),
 	}},
