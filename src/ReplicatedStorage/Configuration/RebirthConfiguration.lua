188ef16cd712da82f8c4d966a6cd8a4971419ee2
GulraizRocketFuel <gulraiz.rocketfuel@gmail.com>
2026-03-30 17:32:42 +0500

    UPDATE adjust rebirth money requirements and rewards

--- src/ReplicatedStorage/Configuration/RebirthConfiguration.lua
+++ src/ReplicatedStorage/Configuration/RebirthConfiguration.lua
@@ -2,10 +2,10 @@ return {
 	MULTI_INCREMENT = 0.25,
 
 	REBIRTH = {
-		[1] = { MoneyRequired = 150000, MoneyReward = 7500 },
-		[2] = { MoneyRequired = 5000000, MoneyReward = 250000 },
-		[3] = { MoneyRequired = 10000000, MoneyReward = 500000 },
-		[4] = { MoneyRequired = 500000000, MoneyReward = 25000000 },
-		[5] = { MoneyRequired = 1500000000, MoneyReward = 75000000 },
+		[1] = { MoneyRequired = 1500000, MoneyReward = 5000 },
+		[2] = { MoneyRequired = 10000000, MoneyReward = 20000 },
+		[3] = { MoneyRequired = 75000000, MoneyReward = 100000 },
+		[4] = { MoneyRequired = 600000000, MoneyReward = 600000 },
+		[5] = { MoneyRequired = 5000000000, MoneyReward = 7500000 },
 	},
 }
